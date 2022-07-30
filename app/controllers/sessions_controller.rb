9483cfacdcdc13249dbd59cb4b35b7786d562d20
tsemb012 <tsukasa0171015@gmail.com>
2022-07-30 15:36:40 +0900

    DST-29 永続的なログイン機構を導入(クッキーを用いた認証システム)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       # 成功した場合、user情報を渡し、成功を伝える、
     else
       #エラーメッセージも作成する。モバイル側にエラーとメッセージを送るのか。あるいは通常のメッセージを送るのか？
@@ -15,6 +16,6 @@ class SessionsController < ApplicationController
   #値を送る側は右の形で送る。 { session: { password: "foobar", email: "user@example.com" } }
 
   def destroy
-    log_out
+    log_out if logged_in?
   end
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -19,6 +19,21 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert is_logged_in?
     delete logout_path
     assert_not is_logged_in?
+    delete logout_path
+  end
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_not_empty cookies[:remember_token]
+  end
+
+  test "login without remembering" do
+    # cookieを保存してログイン
+    log_in_as(@user, remember_me: '1')
+    delete logout_path
+    # cookieを削除してログイン
+    log_in_as(@user, remember_me: '0')
+    assert_empty cookies[:remember_token]
   end
 
 end
