f8a685cc3e718a26da6717ee5ba1c383524fe0fd
tsemb012 <65854024+tsemb012@users.noreply.github.com>
2022-11-12 18:59:34 +0900

    DST-386 Resourcesを削除し、個々でUserに対するRoutingを行う。

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   resources :groups
-  resources :users
+  get 'users' => 'users#index'
+  get 'users/:user_id' => 'users#show'
+  get 'users/new' => 'users#new'
+  post 'users' => 'users#create'
+  get 'users/:id/edit' => 'users#edit'
+  patch 'users/:user_id'  => 'users#update'
+  delete 'users/:user_id' => 'users#destroy'
   get '/signup', to: 'users#sign_up'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
