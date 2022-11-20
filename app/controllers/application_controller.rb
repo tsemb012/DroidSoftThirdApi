e7f18455730b77b01522153cab87330346d73d49
tsemb012 <65854024+tsemb012@users.noreply.github.com>
2022-11-20 19:47:26 +0900

    DST-428 add: func to refresh token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,10 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
   include SessionsHelper
-  before_action :authenticate
+  before_action :certify_token
   attr_accessor :email
   class AuthenticationError < StandardError; end
   rescue_from AuthenticationError, with: :not_authenticated
-
-  def authenticate
-    token = FirebaseToken.new(request.headers["Authorization"])
-    firebase_project_id = 'droidsoftthird-2cc9b'
-    payload = token.verify(firebase_project_id)
-    @email = payload["email"]
-  end
 
 =begin # なくても困らないようであれば削除するように
   def current_user(user_id = nil) # TODO これは誰が使っているコードなのか？ いらないなら消してしまう。
@@ -20,7 +13,20 @@ class ApplicationController < ActionController::API
 =end
 
   private
-  def not_authenticated
-    render json: { error: { messages: ["ログインしてください"] } }, status: :unauthorized
+
+  def certify_token
+    begin
+      authenticate
+    rescue JWT::DecodeError, JWT::ExpiredSignature, JWT::VerificationError
+      render json: { message: 'unauthorized' }, status: 401
+    end
   end
+
+  def authenticate
+    token = FirebaseToken.new(request.headers["Authorization"])
+    firebase_project_id = 'droidsoftthird-2cc9b'
+    payload = token.verify(firebase_project_id)
+    @email = payload["email"]
+  end
+
 end
