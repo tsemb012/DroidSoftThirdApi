61a9d0168c5297560e2d66af2082154b549bd0de
tsemb012 <tsukasa0171015@gmail.com>
2023-02-19 17:22:50 +0900

    DST-446 登録の処理をトランザクションで行うように

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,34 +1,31 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :update, :destroy, :participate]
 
-  # POST /events
   def create
     event = Event.new(event_params)
     place = Place.new(place_params)
-    event.save!
     event.place = place
-    if place.save!
-      ActiveRecord::Base.transaction do
-        event.place = place
-        event.users << User.find_by(user_id: event[:host_id])
-        group = Group.find event[:group_id]
-        group.events << event
-      end
-      render json: { message: "success" }, status: :created
-    else
-      render json: { message: "failure", errors: event.errors.full_messages + place.errors.full_messages }, status: 400
+
+    ActiveRecord::Base.transaction do
+      event.save!
+      event.users << User.find_by(user_id: event.host_id)
+      group = Group.find(event.group_id)
+      group.events << event
     end
+
+    render json: { message: "success" }, status: :created
+  rescue ActiveRecord::RecordInvalid => e
+    render json: { message: "failure", errors: event.errors.full_messages + place.errors.full_messages }, status: 400
   end
 
   private
-  # Use callbacks to share common setup or constraints between actions.
+
   def set_event
     @event = Event.find(params[:id])
   end
 
-  # Only allow a trusted parameter "white list" through.
   def event_params
-    params.require(:event).permit(:host_id, :name, :comment, :date, :start_time, :end_time, :place_id, :group_id)
+    params.require(:event).permit(:host_id, :name, :comment, :date, :start_time, :end_time, :group_id)
   end
 
   def place_params
