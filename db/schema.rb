0d1b6cc79b8adc316a82af82f77c142380d54dcb
tsemb012 <tsukasa0171015@gmail.com>
2023-05-03 21:48:03 +0900

    DST-473 オンラインのブーリアンを追加する

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_28_151608) do
+ActiveRecord::Schema.define(version: 2023_05_03_123447) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2023_04_28_151608) do
     t.string "host_id"
     t.integer "prefecture_code"
     t.integer "city_code"
+    t.boolean "is_online"
   end
 
   create_table "participations", force: :cascade do |t|
